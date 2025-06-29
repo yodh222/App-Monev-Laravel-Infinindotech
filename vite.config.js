03deda9c24cb592ea191e7e67c35be83d00c3676
Yodha Agasthya Novianto Putra <yodhaagasthya07@gmail.com>
2025-06-29 14:51:01 +0700

    Fix vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: 'iks.app-monev-laravel.local',
+        cors: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
